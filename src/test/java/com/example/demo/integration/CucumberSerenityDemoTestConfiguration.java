a7ec4894b68128c2038f556699b30816c8068ac4
Patryk Wojcik <patryk.wojcik@rulefinancial.com>
2019-02-05 15:49:26 +0100

    Support for running tests aganist external env without embeded tomcat

--- src/test/java/com/example/demo/integration/CucumberSerenityDemoTestConfiguration.java
+++ src/test/java/com/example/demo/integration/CucumberSerenityDemoTestConfiguration.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Profile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
